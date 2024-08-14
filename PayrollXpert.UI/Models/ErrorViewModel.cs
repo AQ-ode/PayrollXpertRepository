0eb2ea288dac1b68135d07116fdb9e8709e6b454
Ammar <smakali1999@gmail.com>
2024-08-14 15:24:05 +0500

    Add project files.

---
+++ PayrollXpert.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PayrollXpert.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
